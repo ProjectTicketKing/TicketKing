c808559b3e3675e5fc4171a0011a68a100414eb9
seol11796 <seol11796@gmail.com>
2023-06-22 15:24:53 +0900

    initialized configuration

---
+++ ticketKing/src/main/java/com/example/ticketKing/TicketKingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketKing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketKingApplication.class, args);
+	}
+
+}
