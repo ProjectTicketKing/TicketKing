4a8e760611c386c53339703c8489ce0809fcfc16
parkchaewon18 <0101angela@naver.com>
2023-06-23 15:58:47 +0900

    Feat: HomeController

---
+++ ticketKing/src/main/java/com/example/ticketKing/domain/Home/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.ticketKing.domain.Home.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String showMain() {
+        return "usr/main/home";
+    }
+
+}
