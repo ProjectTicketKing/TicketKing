893a11bba0e0fb49e6170d1674e4e32715e28a04
seol11796 <seol11796@gmail.com>
2023-07-05 15:24:43 +0900

    wait line function added

--- ticketKing/src/main/resources/static/js/timer.js
+++ ticketKing/src/main/resources/static/js/timer.js
@@ -1,5 +1,15 @@
 var timerInterval;
 var isfinished = false;
+var queueCount = 0; // 대기열 고객 수
+var startTime; // Variable to store the start time
+var timeDifference;
+
+// Event listener for DOMContentLoaded to record the start time when the button is loaded
+document.addEventListener("DOMContentLoaded", function() {
+
+});
+
+
 
 function startGame() {
     var timerDisplay = document.getElementById('timer');
@@ -8,21 +18,21 @@ function startGame() {
     var selectedTime = parseInt(timeSelect.value, 10);
 
     if (isNaN(selectedTime)) {
-        timerDisplay.innerText = '00 : 00 : 00'; // 시간을 선택하지 않았을 경우 시간 표시를 00:00:00으로 설정
-        return; // 함수 종료
+        timerDisplay.innerText = '00 : 00 : 00';
+        return;
     }
 
     var seconds = selectedTime;
     timerDisplay.innerText = formatTime(seconds);
 
-    startButton.setAttribute('disabled', true); // 버튼 비활성화
+    startButton.setAttribute('disabled', true);
 
-    clearInterval(timerInterval); // 이전 타이머 중지
+    clearInterval(timerInterval);
+
     timerInterval = setInterval(function() {
-        if(isfinished) {
+        if (isfinished) {
             seconds++;
-        }
-        else{
+        } else {
             seconds--;
         }
         timerDisplay.innerText = formatTime(seconds);
@@ -30,9 +40,16 @@ function startGame() {
         if (seconds <= 0) {
             isfinished = true;
             startButton.innerText = '예매하기';
-            startButton.removeAttribute('disabled'); // 버튼 활성화
+            startTime = new Date();
+            startButton.removeAttribute('disabled');
             startButton.onclick = function() {
-                window.location.href = '/usr/concert/date'; // 예매 페이지로 이동하는 로직 추가
+                var endTime = new Date();
+                timeDifference = endTime - startTime;
+                console.log("Time taken to click Start button:", timeDifference + " ms");
+
+                openModal();
+
+                showMessage(); // 메시지를 보여주는 함수 호출
             };
         }
     }, 1000);
@@ -49,4 +66,66 @@ function formatTime(seconds) {
 
 function padZero(value) {
     return value < 10 ? '0' + value : value;
+}
+
+function openModal() {
+    var modal = document.getElementById('modal');
+    var queueCountElement = document.getElementById('queueCount');
+
+    // 대기열 수 증가
+    queueCount++;
+    queueCountElement.innerText = queueCount;
+
+    // 대기열 모달 창 열기
+    modal.style.display = 'flex';
+
+      // 타이머가 종료되면 모달을 자동으로 닫도록 설정
+      if (timeDifference >= 0 && timeDifference < 300) {
+          timerInterval = setInterval(function() {
+              closeModal();
+          }, 10000); // 10초 후에 자동으로 닫힘 (대기 시간)
+      } else if (timeDifference >= 300 && timeDifference < 500) {
+          timerInterval = setInterval(function() {
+              closeModal();
+          }, 2000); // 20초 후에 자동으로 닫힘 (대기 시간)
+      } else if (timeDifference >= 500) {
+          timerInterval = setInterval(function() {
+              closeModal();
+          }, 3000); // 30초 후에 자동으로 닫힘 (대기 시간)
+      }
+
+    // 모달 닫기 버튼 클릭 이벤트
+    var modalCloseButton = document.getElementById('modalCloseButton');
+    modalCloseButton.onclick = function() {
+      clearInterval(timerInterval); // 타이머 중지
+      closeModal(); // 모달 창 닫기
+    };
+}
+
+function closeModal() {
+    var modal = document.getElementById('modal');
+    var queueCountElement = document.getElementById('queueCount');
+
+    // 대기열 수 초기화
+    queueCount = 0;
+    queueCountElement.innerText = queueCount;
+
+    // 대기열 모달 창 닫기
+    modal.style.display = 'none';
+
+    // 다른 페이지로 이동 (여기서는 '예매 페이지'로 이동하도록 설정)
+      window.location.href = '/usr/concert/date'; // 예매 페이지로 이동하는 로직 추가
+}
+
+// timeDifference에 따라 다른 메시지를 보여주는 함수
+function showMessage() {
+    var queueCountElement = document.getElementById('queueCount');
+
+    if (timeDifference >= 0 && timeDifference < 300) {
+        queueCountElement.innerText = parseInt(queueCountElement.innerText) + 33;
+    } else if (timeDifference >= 300 && timeDifference < 500) {
+        queueCountElement.innerText = parseInt(queueCountElement.innerText) + 120;
+    } else if (timeDifference >= 500) {
+        queueCountElement.innerText = parseInt(queueCountElement.innerText) + 1300;
+    }
 }
