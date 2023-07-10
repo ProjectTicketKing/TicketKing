473e6968c9223e9ea2e96bc67fb245e258ac3d15
seol11796 <seol11796@gmail.com>
2023-07-10 11:14:39 +0900

    url connect

--- ticketKing/src/main/java/com/example/ticketKing/domain/Hall/controller/HallController.java
+++ ticketKing/src/main/java/com/example/ticketKing/domain/Hall/controller/HallController.java
@@ -2,6 +2,7 @@ package com.example.ticketKing.domain.Hall.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 public class HallController {
     @GetMapping("/usr/concert/{hall}/stage")
-    public String showConcertSeatArea(@PathVariable("hall")String hallName) {
+    public String showConcertSeatArea(Model model, @PathVariable("hall")String hallName) {
+        model.addAttribute("hallName",hallName);
         return "usr/hall/"+hallName;
-    }
-
+          }
 
 }
