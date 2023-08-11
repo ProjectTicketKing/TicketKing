f398aeb56c6cf5fab1663e8dbe2950c0cf203246
parkchaewon18 <0101angela@naver.com>
2023-08-11 14:54:04 +0900

    Refact: 가장 최신 좌석 데이터 전송

--- ticketKing/src/main/java/com/example/ticketKing/domain/Practice/repository/PracticeRepository.java
+++ ticketKing/src/main/java/com/example/ticketKing/domain/Practice/repository/PracticeRepository.java
@@ -4,8 +4,11 @@ import com.example.ticketKing.domain.Practice.entity.Practice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PracticeRepository extends JpaRepository<Practice,Long> {
 
     List<Practice> findByMemberId(Long memberId);
+    // 추가 메서드
+    Optional<Practice> findTopByMemberIdOrderBySeatSelectionTimeDesc(Long memberId);
 }
